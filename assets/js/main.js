8fd62b8d712fbf4208c406b621126ba3d8ad9a44
Maca <maca.villa.a@gmail.com>
2019-04-06 22:04:31 -0300

    dots y custom paging

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,33 +3,34 @@ $(function() {
   $('#schedule').slick({
     infinite: false,
     arrows: false,
-    // customPaging: function (slider, i) {
-    //   switch (i) {
-    //     case 0:
-    //       return '<button class=\'tab\' onclick="ga(\'send\', \'event\', \'CTA\', \'click\', \'19CY1_WIM_BTS_WBubbleIM_LT_D__LandingPage_click_load_WIM_BTS_POR_Desempenho\')"><span></span></button>';
-    //       break;
+    dots: true,
+    customPaging: function (slider, i) {
+      switch (i) {
+        case 0:
+          return '<button class=\'tab\'><span></span></button>';
+          break;
 
-    //     case 1:
-    //       return '<button class=\'tab\' onclick="ga(\'send\', \'event\', \'CTA\', \'click\', \'19CY1_WIM_BTS_WIM_LT_D_Bubble_LandingPage_click_load_WIM_BTS_POR_Bateria\')"><span></span></button>';
-    //       break;
+        case 1:
+          return '<button class=\'tab\' ><span></span></button>';
+          break;
 
-    //     case 2:
-    //       return '<button class=\'tab\' onclick="ga(\'send\', \'event\', \'CTA\', \'click\', \'19CY1_WIM_BTS_WIM_LT_D_Bubble_LandingPage_click_load_WIM_BTS_POR_Mobilidade\')"><span></span></button>';
-    //       break;
+        case 2:
+          return '<button class=\'tab\' "><span></span></button>';
+          break;
 
-    //     case 3:
-    //       return '<button class=\'tab\' onclick="ga(\'send\', \'event\', \'CTA\', \'click\', \'19CY1_WIM_BTS_WIM_LT_D_Bubble_LandingPage_click_load_WIM_BTS_POR_Graficos\')"><span></span></button>';
-    //       break;
+        case 3:
+          return '<button class=\'tab\'"><span></span></button>';
+          break;
 
-    //     case 4:
-    //       return '<button class=\'tab\' onclick="ga(\'send\', \'event\', \'CTA\', \'click\', \'19CY1_WIM_BTS_WIM_LT_D_Bubble_LandingPage_click_load_WIM_BTS_POR_Resposta\')"><span></span></button>';
-    //       break;
+        case 4:
+          return '<button class=\'tab\'><span></span></button>';
+          break;
 
-    //     default:
-    //       return '<button class=\'tab\'><span></span></button>';
-    //       break;
-    //   }
-    // },
+        default:
+          return '<button class=\'tab\'><span></span></button>';
+          break;
+      }
+    },
     
   });
 });
