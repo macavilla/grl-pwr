9252d766d1efe50e4322e7bc8533e49158f044ea
Maca <maca.villa.a@gmail.com>
2019-04-06 22:41:46 -0300

    customPaging styles

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,26 +4,27 @@ $(function() {
     infinite: false,
     arrows: false,
     dots: true,
+    appendDots: $('#customDots'),
     customPaging: function (slider, i) {
       switch (i) {
         case 0:
-          return '<button class=\'tab\'><span></span></button>';
+          return '<button class=\'tab\'><span>ESCENARIO</span>VIOLETA</button>';
           break;
 
         case 1:
-          return '<button class=\'tab\' ><span></span></button>';
+          return '<button class=\'tab\' ><span>ESCENARIO</span> VERDE</button>';
           break;
 
         case 2:
-          return '<button class=\'tab\' "><span></span></button>';
+          return '<button class=\'tab\' "><span>ESCENARIO</span> SOFAR</button>';
           break;
 
         case 3:
-          return '<button class=\'tab\'"><span></span></button>';
+          return '<button class=\'tab\'"><span>TEATRO</span></button>';
           break;
 
         case 4:
-          return '<button class=\'tab\'><span></span></button>';
+          return '<button class=\'tab\'><span>SUM</span></button>';
           break;
 
         default:
